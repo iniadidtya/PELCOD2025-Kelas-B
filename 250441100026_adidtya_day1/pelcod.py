0beabe2368fcc08aa12d88258c393ddb02f35e6b
haloadidtya <m.adidtya.pr@gmail.com>
2025-09-13 11:35:24 +0700

    menambahkan pelcod day 1

---
+++ 250441100026_adidtya_day1/pelcod.py
@@ -0,0 +1,29 @@
+# nama = "Muhammad Adidtya Putra Ramadhan"
+# nim = "250441100026"
+# semester = 1
+
+# print("====BIODATA MAHASISWA====")
+# print("Hallo, nama saya"+ nama + "\n")
+# print(f"NIM saya : {nim}\n")
+# print("Saya berkuliah di semester : {}".format(semester))
+
+# a = 10
+# b = 5
+# c = a + b #Penjumlahan
+# d = a - b #Pengurangan
+# e = a * b #Perkalian
+# f = a / b #Pembagian
+# g = a % b #Sisa Bagi
+# print("Hasil Penjumlahan" , c)
+# print("Hasil Pengurangan" , d)
+# print("Hasil Perkalian" , e)
+# print("Hasil Pembagian" , f)
+# print("Hasil Sisa Bagi" , g)
+
+nama = input("Masukan nama Anda:")
+umur = input("Masukan umur Anda:")
+buah = input("Masukan buah favorit Anda:")
+
+print("Nama saya adalah " + nama + nama)
+print("Umur saya adalah " + umur + " tahun")
+print("buah favorit saya adalah " + buah)
